bc1aedccb5cc390999ae6ff248ab2ec980aab02f
Santiago Pravisani <santiagopravisani@gmail.com>
2015-01-13 23:04:13 -0300

    Primer vistazo

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ Bundler.require :default, ENV['RACK_ENV'].to_sym
 Dir['./models/**/*.rb'].each {|f| require f }
 
 class Application < Sinatra::Base
+
   register Sinatra::ActiveRecordExtension
 
   configure :production, :development do
@@ -15,8 +16,19 @@ class Application < Sinatra::Base
   set :database, YAML.load_file('config/database.yml')[ENV['RACK_ENV']]
 
   get '/' do
-    @greet = 'Hello from sinatra!'
+  	erb :login
+  end
 
-    erb 'hello/greet'.to_sym
+  post '/auth/login' do
+   	#User.new(params['user']['username'],params['user']['password'])
+  end
+
+  get '/users' do
+  	User.create(username:"forrochan")
+  end
+
+  get '/register' do
+  	"Registro de usuario"
   end
 end
+
---
+++ db/migrate/20150114005159_create_users.rb
@@ -0,0 +1,9 @@
+#bundle exec rake db:migrate
+class CreateUsers < ActiveRecord::Migration
+  def change
+  	create_table :users do |t|
+  		t.string :username
+  	end
+
+  end
+end
